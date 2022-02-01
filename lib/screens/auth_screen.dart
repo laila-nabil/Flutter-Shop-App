c2f0ea797efc5ee0d82b29fd7da8d6975b082690
laila.nabil <laila.n.mostafa@gmail.com>
2022-02-01 02:41:17 +0200

    web version

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -182,7 +182,7 @@ class _AuthCardState extends State<AuthCard>
         curve: Curves.easeIn,
         constraints:
             BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
+        width: deviceSize.width < 500 ? deviceSize.width * 0.75 :  370,
         padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
