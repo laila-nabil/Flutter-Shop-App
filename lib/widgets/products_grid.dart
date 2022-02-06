be72dfe01b77bdc8a606479716d6b5bc04e7248c
laila.nabil <laila.n.mostafa@gmail.com>
2022-02-06 20:31:53 +0200

    adjust no of products in products_grid

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -16,13 +16,16 @@ class ProductsGrid extends StatelessWidget {
     final productsData = Provider.of<Products>(context);
     final loadedProducts = showFav ? productsData.favItems : productsData.items;
     final screenSize = MediaQuery.of(context).size;
+    print("screenSize width ${screenSize.width}");
+    print("screenSize height ${screenSize.height}");
     final isPortrait = screenSize.width < screenSize.height;
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        childAspectRatio: isPortrait ? 2 / 3 : 1,
+        // childAspectRatio: isPortrait ? 2 / 3 : 1,
+        childAspectRatio: screenSize.width <= (screenSize.height * 0.65) ? 2 / 3 : 1,
         // crossAxisCount: isPortrait ? 2 : 3,
-        crossAxisCount: screenSize.width <= screenSize.height/2 ? 2 : screenSize.width < 800 ? 3 : 5,
+        crossAxisCount: screenSize.width <= (screenSize.height * 0.65)? 2 : screenSize.width < 800 ? 3 : 5,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
       ),
